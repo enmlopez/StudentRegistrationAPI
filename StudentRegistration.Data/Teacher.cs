4284c343e3911b79c21b502476f80beaf786091d
dawita <dawit.fitru@gmail.com>
2021-02-23 21:53:33 -0500

    test

--- StudentRegistration.Data/Teacher.cs
+++ StudentRegistration.Data/Teacher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace StudentRegistration.Data
 {
     public class Teacher
     {
+        [ForeignKey(nameof(Course))]
+        public int? CourseId { get; set; }
+        public virtual Course Course { get; set; }
     }
 }
---
+++ StudentRegistration.Models/TeacherListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Models
+{
+    public class TeacherListItem
+    {
+        public int? CourseId { get; set; }
+        public string CourseTitle { get; set; }
+    }
+}
